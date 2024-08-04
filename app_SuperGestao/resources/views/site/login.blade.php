a8cca06cc1a13c4de58afdcb96537da795493d12
Oliver <talis.oliveira@hotmail.com.br>
2024-08-04 20:07:45 -0300

    comecando secao 11

--- app_SuperGestao/resources/views/site/login.blade.php
+++ app_SuperGestao/resources/views/site/login.blade.php
@@ -1,23 +1,41 @@
-<h3>Login View</h3>
+@extends('site.layouts.basico')
 
-<ul>
-    <li>
-        <a href="{{ route('site.index') }}">Principal</a>
-    </li>
-    <li>
-        <a href="{{ route('site.sobrenos') }}">Sobre nós</a>
-    </li>
-    <li>
-        <a href="{{ route('site.contato') }}">Contato</a>
-    </li>
-    <ol>App</ol>
-    <li>
-        <a href="{{ route('app.clientes') }}">Clientes</a>
-    </li>
-    <li>
-        <a href="{{ route('app.fornecedores') }}">Fornecedores</a>
-    </li>
-    <li>
-        <a href="{{ route('app.produtos') }}">Produtos</a>
-    </li>
-</ul>
+@section('titulo', $titulo)
+
+@section('conteudo')
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina">
+            <h1>Login</h1>
+        </div>
+
+        <div class="informacao-pagina">
+            <div style="width:30%; margin-left: auto; margin-right: auto;">
+                <form action={{ route('site.login') }} method="post">
+                    @csrf
+                    <input name="usuario" type="text" placeholder="Usuário" class="borda-preta">
+                    <input name="senha" type="password" placeholder="Senha" class="borda-preta">
+                    <button type="submit" class="borda-preta">Acessar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="rodape">
+        <div class="redes-sociais">
+            <h2>Redes sociais</h2>
+            <img src="{{ asset('img/facebook.png') }}">
+            <img src="{{ asset('img/linkedin.png') }}">
+            <img src="{{ asset('img/youtube.png') }}">
+        </div>
+        <div class="area-contato">
+            <h2>Contato</h2>
+            <span>(11) 3333-4444</span>
+            <br>
+            <span>supergestao@dominio.com.br</span>
+        </div>
+        <div class="localizacao">
+            <h2>Localização</h2>
+            <img src="{{ asset('img/mapa.png') }}">
+        </div>
+    </div>
+@endsection
--- app_SuperGestao/routes/web.php
+++ app_SuperGestao/routes/web.php
@@ -14,7 +14,8 @@ use App\Http\Controllers\PosContatoController;
 Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
-Route::get('/login', [LoginController::class, 'login'])->name('site.login');
+Route::get('/login', [LoginController::class, 'index'])->name('site.login');
+Route::post('/login', [LoginController::class, 'autenticar'])->name('site.login');
 Route::get('/pos-contato', [PosContatoController::class, 'posContato'])->name('site.pos-contato');
 
 // Regular POST Routes
