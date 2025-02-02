94bb44d858f631f804b407267e7c7b2f3fd90219
Oliver <talis.oliveira@hotmail.com.br>
2025-02-02 15:32:21 -0300

    task parte 1

---
+++ Teste/project/database/migrations/2025_02_02_182350_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id('taskId');
+            $table->unsignedBigInteger('userID');
+            $table->text('taskDescription');
+            $table->enum('taskStatus', ['P', 'C']);
+            $table->enum('taskActive', ['S', 'N']);
+            $table->timestamps();
+    
+            $table->foreign('userID')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
--- Teste/project/routes/web.php
+++ Teste/project/routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -26,6 +24,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('tasks', TaskController::class);
 });
 
 require __DIR__.'/auth.php';
